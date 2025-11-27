727c2ba106738219a2d700cf990b238c4458bc56
Bhavesh Patil <bhaveshpatil918@gmail.com>
2025-11-27 07:44:40 +0530

    feat: add initial application structure with client and server entry points and TanStack Router configuration.

---
+++ app/client.tsx
@@ -0,0 +1,8 @@
+/// <reference types="vinxi/types/client" />
+import { hydrateRoot } from 'react-dom/client'
+import { StartClient } from '@tanstack/react-start'
+import { getRouter } from '../src/router'
+
+const router = getRouter()
+
+hydrateRoot(document, <StartClient router={router} />)
